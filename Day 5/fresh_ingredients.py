e068182bc8c915e959ef5f757fd9789b6b22a74d
Nathaniel Rodriguez <golansy0@gmail.com>
2025-12-08 13:55:46 -0800

    Day 5 complete

--- Day 5/fresh_ingredients.py
+++ Day 5/fresh_ingredients.py
@@ -33,14 +33,33 @@ def num_fresh_ingredients(file_name: str) -> int:
                 break
     return count
 
+def adjust_ranges(ranges: list[tuple[int, int]]) -> list[tuple[int, int]]:
+    adjusted_ranges = []
+    for i in range(len(ranges)):
+        range_start, range_end = ranges[i]
+        j = 0
+        while j < len(adjusted_ranges):
+            rs, re = adjusted_ranges[j]
+            if rs <= range_start <= re:
+                range_start = re + 1
+            if rs <= range_end <= re:
+                range_end = rs - 1
+            if range_start <= rs <= range_end:
+                adjusted_ranges.pop(j)
+            else:
+                j += 1
+        if range_start <= range_end:
+                adjusted_ranges.append((range_start, range_end))
+    return adjusted_ranges
+
 def num_fresh_ingredients_in_range(file_name: str) -> int:
     ranges = []
     parse_input(file_name, ranges)
-    ids = set()
+    ranges = adjust_ranges(ranges)
+    count = 0
     for range_start, range_end in ranges:
-        for i in range(range_start, range_end+1):
-            ids.add(i)
-    return len(ids)
+        count += range_end - range_start + 1
+    return count
 
 print(num_fresh_ingredients("input.txt"))
 print(num_fresh_ingredients_in_range("input.txt"))
